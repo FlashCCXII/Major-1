497572ef8c4caa057982c52b9eaf64a8ff33e48b
olusegun1234 <noahcoon@my.unt.edu>
2024-10-29 18:10:49 -0500

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int reverseBits(int num) {
+        unsigned int reversed = 0;
+        
+
+        for (int i = 0; i < 32; i++) {
+
+            reversed <<= 1;
+            reversed |= (num & 1);
+            num >>= 1;
+        }
+
+        printf("%d Reversed bits result is: %u\n", reversed);
+        
+        return reversed;
+        
+        }
