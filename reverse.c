9e3f18a98fa58ef643f94ef1eadd475b697ea069
DOAjayi <125410155+FlashCCXII@users.noreply.github.com>
2024-10-29 23:57:27 -0500

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include "major1.h"
 
-unsigned int reverse(int num) {
-        unsigned int reversed = 0;
-        
+unsigned int reverse(unsigned int num) {
+    unsigned int reversed = 0;
+    unsigned int original = num;
 
-        for (int i = 0; i < 32; i++) {
+    for (int i = 0; i < 32; i++) {
+        reversed <<= 1;             
+        reversed |= (num & 1);    
+        num >>= 1;                
+    }
 
-            reversed <<= 1;
-            reversed |= (num & 1);
-            num >>= 1;
-        }
-
-        printf("%d Reversed bits result is: %u\n", reversed);
-        
-        return reversed;
-        
-        }
+    printf("%u with bits reversed is: %u\n", original, reversed);
+    
+    return reversed;
+}
