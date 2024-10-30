61b643bf0c31ca9f682cbec2815fb76a4234b230
DOAjayi <125410155+FlashCCXII@users.noreply.github.com>
2024-10-29 22:34:20 -0500

    Update palindrome.c
    changed back to int

--- palindrome.c
+++ palindrome.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "major1.h"
 
-void palindrome(int num){
+int palindrome(int num){
     while (num<=0 || num >= 2000000000){
         printf("Enter a positive integer less than 2 billion: ");
         scanf("%d",&num);
