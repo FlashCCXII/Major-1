f67123253c82f94f7d615864bcff2fd269a2c625
DOAjayi <125410155+FlashCCXII@users.noreply.github.com>
2024-10-29 20:41:14 -0500

    Add files via upload

---
+++ major1.h
@@ -0,0 +1,15 @@
+#ifndef MAJOR1_H
+#define MAJOR1_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu();
+unsigned int getNumber(char *prompt, unsigned int max);
+void createPalindrome(unsigned int num);
+void power(unsigned int num);
+unsigned int reverse(unsigned int num);
+void replace(unsigned int num);
+void palindrome(unsigned int num);
+
+#endif
