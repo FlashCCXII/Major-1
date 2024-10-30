4a7ffaf3aff7d1d5b1fd18d78a13eb5c0e6183df
DOAjayi <125410155+FlashCCXII@users.noreply.github.com>
2024-10-29 23:56:45 -0500

    Update major1.c

--- major1.c
+++ major1.c
@@ -9,17 +9,17 @@ int main() {
 
     while(1) {
         choice = getNumber("\nEnter your choice: ", 10);
-        if (choice != 10 && choice != 0)
-            num = getNumber("Enter a number: ", 2000000000);
-
+        if (choice >= 1  && choice <= 5)
+            num = getNumber("Enter a positive integer less than 2 billion: ", 2000000000);
+        else {
+            choice = 0;
+        }
         switch (choice) {
-            case 0:     menu();                 break;
-            case 1:     printf ("createPalindrome\n");      createPalindrome(num);  break;
-            case 2:     printf ("power\n");                 power(num);             break;
-            case 3:     printf ("reverse\n");               reverse(num);           break;
-            case 4:     printf ("replace\n");               replace(num);           break;
-            case 5:     printf ("palindrome\n");            palindrome(num);        break;
-            case 10:    return 0;
+            case 1:     power(num);             break;
+            case 2:     reverse(num);           break;
+            case 3:     replace(num);           break;
+            case 4:     palindrome(num);        break;
+            case 5:     printf ("Exiting program...");       return 0;
             default:    printf ("Invalid choice. Please try again.\n");
         }
     }
@@ -28,12 +28,11 @@ int main() {
 
 void menu() {
     printf("\nMenu:\n");
-    printf("1.  Create a Palindrome\n");
-    printf("2.  Power\n");
-    printf("3.  Reverse\n");
-    printf("4.  Replace\n");
-    printf("5.  Palindrome\n");
-    printf("10. Exit\n");
+    printf("1.  Power\n");
+    printf("2.  Reverse\n");
+    printf("3.  Replace\n");
+    printf("4.  Palindrome\n");
+    printf("5. Exit\n");
 }
 
 unsigned int getNumber(char *prompt, unsigned int max) {
@@ -47,7 +46,12 @@ unsigned int getNumber(char *prompt, unsigned int max) {
         if (num <= max) {
             return num;
         }
-        printf("Input exceed maximum allowed value. Please try again.\n");
+        else if (num <= 0){
+            printf("Input does not reach minimum allowed value. Please try again.\n");
+        }
+        else {
+        printf("Input exceeds maximum allowed value. Please try again.\n");
+        }
     }
 }
 
